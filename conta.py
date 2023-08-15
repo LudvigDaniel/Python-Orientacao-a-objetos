53a8ef6247b972bf93659b99d68275862531bd0c
Daniel M Ludvig <Danielmludvig@gmail.com>
2023-08-15 16:56:58 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -4,19 +4,37 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite 
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite 
         
     def extrato(self):
-        print("Saldo do titular Sr(a){} é de R${}".format(self.titular, self.saldo))
+        print("Saldo do titular Sr(a){} é de R${}".format(self.__titular, self.__saldo))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saque(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
+    def transferir(self, valor, destino):
+        self.saque(valor)
+        destino.deposita(valor)
 
-        
+    def get_saldo(self):    
+        return self.__saldo
+    
+    def get_titular(self):
+        return self.__titular
+    
+    def get_limite(self):
+        return self.__limite
+    
+    def get_conta(self):
+        return self.__numero
+    
+    def set_limite(self, limite):
+        self.__limite = limite
+
+    
